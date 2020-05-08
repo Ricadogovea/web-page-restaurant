02cc53fbab7bf74398a1de4d1a8fcbc603721800
Ricadogovea <60119759+Ricadogovea@users.noreply.github.com>
2020-05-08 10:07:37 -0500

    1
    1

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(function () {
+	$("navbarToggle").blur(function (event){
+		var screenWidth = window.innerWidth;
+		if (screenWidth < 768) {
+	    $("#collapsable-nav").collapse('hide');
+		}
+
+	}
+    )
+})
+
+
+// Remove the class 'active' from home and switch to Menu button
+var switchMenuToActive = function () {
+  // Remove 'active' from home button
+  var classes = document.querySelector("#navHomeButton").className;
+  classes = classes.replace(new RegExp("active", "g"), "");
+  document.querySelector("#navHomeButton").className = classes;
+
+  // Add 'active' to menu button if not already there
+  classes = document.querySelector("#navMenuButton").className;
+  if (classes.indexOf("active") == -1) {
+    classes += " active";
+    document.querySelector("#navMenuButton").className = classes;
+  }
+};
